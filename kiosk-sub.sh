650a7f569dadf8b557aa4ee878d8758a83a75bb0
shaunparris <shaunparris@gmail.com>
2018-04-17 12:44:59 -0600

    extended sleeps to enable pages to load

--- kiosk-sub.sh
+++ kiosk-sub.sh
@@ -19,7 +19,7 @@ chromium-browser --kiosk --disable-session-crashed-bubble --disable-infobars --s
 sleep 30
 
 xdotool key ctrl+Tab
-sleep 30
+sleep 45
 xdotool key ctrl+F6
 sleep 5
 for VARIABLE in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22

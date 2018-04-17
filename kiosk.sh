567926aac93d47207ef90f11deee93df5ef7b464
shaunparris <shaunparris@gmail.com>
2018-04-17 11:47:34 -0600

    Modified to only kill running scripts and chromium, then moved the meat of the script to kiosk-sub.sh. This was to ensure that multiple instances of the scripts won't be running and colliding navigation.

--- kiosk.sh
+++ kiosk.sh
@@ -1,44 +1,14 @@
 #! /bin/bash
 # this will kill all chromium browsers running
 
+killall navigation.sh
+killall kiosk-sub.sh
+killall kiosk.sh
 killall chromium-browser
-
-killall chromium-browser
-
 sleep 5
+bash /home/pi/kiosk-sub.sh
 
-rm -rf /home/pi/.config/chromium/Default/Web\ Data
 
-sleep 2
-
-# Since running from CLI, chromium doesn't know where to display, this fixes that
-
-export DISPLAY=:0.0
-
-#These settings are to prevent the "chrome crashed" messages
-sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/google-chrome/Default/Preferences
-sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/google-chrome/Default/Preferences
-
-# This next step will fire up chromium full-screen with two tabs open
-
-chromium-browser --kiosk --disable-session-crashed-bubble --disable-infobars --start-maximized --noerrdialogs --enable-logging --v=1 https://trello.com/b/WJWGkyKU/buildit-us https://wipro365-my.sharepoint.com/:p:/r/personal/sh20000592_wipro_com/_layouts/15/Doc.aspx?sourcedoc=%7Ba8855340-7a70-45ee-ae55-2ebe73a305f1%7D&action=edit &
-
-sleep 20
-
-xdotool key ctrl+Tab
-sleep 30
-xdotool key ctrl+F6
-sleep 5
-for VARIABLE in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
-do
-        echo "hitting tab key"
-        xdotool key Tab
-        sleep 1
-done
-xdotool key KP_Enter
-echo "Waiting 45 to start navigation script"
-sleep 45
-bash /home/pi/navigation.sh
 
 
 

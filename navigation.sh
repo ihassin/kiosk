35e9a447915e38361836c22f06c0ce893b704187
shaunparris <shaunparris@gmail.com>
2018-04-19 14:58:09 -0600

    Modified navigation

--- navigation.sh
+++ navigation.sh
@@ -6,13 +6,6 @@ sleep 2
 xdotool key ctrl+r
 echo "Sleep 10 for page load"
 sleep 10
-echo "Start moving forward through slides"
-for VARIABLE in 00 01 02 03 04 05
-do
-	xdotool key n
-	echo "Next Slide"
-	sleep 10
-done
 echo "starting infinite loop of scrooling and tabbing"
 for (( ; ; ))
 do
@@ -28,6 +21,21 @@ do
 		for VARIABLE in 00 01 02 03 04 05
         	do
                 xdotool key n
+		echo "Next Slide"
+                sleep 10
+        	done
+    xdotool key ctrl+Tab
+	echo "Sleeping for 1 Minute to display trello board"
+	sleep 30
+	echo "Switching back to presentation"
+	xdotool key ctrl+Tab
+	echo "sleeping for 30 for screen load"
+	sleep 30
+	echo "Waiting for full screen to load - 20"
+	sleep 20
+		for VARIABLE in 00 01 02 03 04 05
+        	do
+                xdotool key p
 		echo "Next Slide"
                 sleep 10
         	done

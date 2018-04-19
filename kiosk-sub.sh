ee86ab405c9b9783da68aa96a003ad02057172f2
shaunparris <shaunparris@gmail.com>
2018-04-19 12:12:03 -0600

    Trying to update presentation URL. Not sure why it's not working.

--- kiosk-sub.sh
+++ kiosk-sub.sh
@@ -13,7 +13,7 @@ sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/google-chro
 sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/google-chrome/Default/Preferences
 
 # This next step will fire up chromium full-screen with two tabs open
-
+#Does this work?
 chromium-browser --kiosk --disable-session-crashed-bubble --disable-infobars --start-maximized --noerrdialogs --enable-logging --v=1 https://trello.com/b/WJWGkyKU/buildit-us file:///home/pi/office%20presentation/assets/player/KeynoteDHTMLPlayer.html &
 echo "Sleeping 30 for page load"
 sleep 30

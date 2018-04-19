2766718f4d7406aee4ca0348afb19cd7917938b5
shaunparris <shaunparris@gmail.com>
2018-04-19 15:09:39 -0600

    Modified navigation to not switch from trello to pres back to trello at the beginning

--- navigation.sh
+++ navigation.sh
@@ -4,19 +4,10 @@
 xdotool key Escape
 sleep 2
 xdotool key ctrl+r
-echo "Sleep 10 for page load"
-sleep 10
 echo "starting infinite loop of scrooling and tabbing"
 for (( ; ; ))
 do
-	xdotool key ctrl+Tab
-	echo "Sleeping for 1 Minute to display trello board"
-	sleep 30
-	echo "Switching back to presentation"
-	xdotool key ctrl+Tab
-	echo "sleeping for 30 for screen load"
-	sleep 30
-	echo "Waiting for full screen to load - 20"
+	echo "Waiting to ensure screen loads - 20"
 	sleep 20
 		for VARIABLE in 00 01 02 03 04 05
         	do
@@ -29,8 +20,6 @@ do
 	sleep 30
 	echo "Switching back to presentation"
 	xdotool key ctrl+Tab
-	echo "sleeping for 30 for screen load"
-	sleep 30
 	echo "Waiting for full screen to load - 20"
 	sleep 20
 		for VARIABLE in 00 01 02 03 04 05
@@ -39,5 +28,9 @@ do
 		echo "Next Slide"
                 sleep 10
         	done
+    xdotool key ctrl+Tab
+    echo "Sleeping for 1 Minute to display trello board"
+	sleep 30
+	xdotool key ctrl+Tab
 done
 echo "This exit message shouldn't be read"

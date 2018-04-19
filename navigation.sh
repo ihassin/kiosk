a5152dce43c3d262458a17ee8e55d105ad095a9c
shaunparris <shaunparris@gmail.com>
2018-04-19 10:26:47 -0600

    modified for new presentation

--- navigation.sh
+++ navigation.sh
@@ -4,7 +4,7 @@
 echo "Sleep for 30 for full screen load"
 sleep 30
 echo "Start moving forward through slides"
-for VARIABLE in 00 01 02 03 04 05 06 07 08 09 10 11
+for VARIABLE in 00 01 02 03 04 05
 do
 	xdotool key n
 	echo "Next Slide"
@@ -16,22 +16,13 @@ do
 	xdotool key ctrl+Tab
 	echo "Sleeping for 1 Minute to display trello board"
 	sleep 30
-	xdotool key ctrl+Tab
 	echo "Switching back to presentation"
+	xdotool key ctrl+Tab
 	echo "sleeping for 30 for screen load"
 	sleep 30
-	echo "starting function to get to full screen again"	
-	xdotool key ctrl+F6
-	echo "f6 ky hit"
-	xdotool key Tab
-	echo "tab key hit"
-	xdotool key shift+Tab
-	echo "shift plus tab kit"
-	xdotool key KP_Enter
-	echo "re-entering fullscreen mode"
 	echo "Waiting for full screen to load - 20"
 	sleep 20
-		for VARIABLE in 00 01 02 03 04 05 06 07 08 09 10 11
+		for VARIABLE in 00 01 02 03 04 05
         	do
                 xdotool key n
 		echo "Next Slide"

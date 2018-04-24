13ffddff7324be585f9e0b4df78a60dbff19c448
shaunparris <shaunparris@gmail.com>
2018-04-23 22:52:58 -0600

    Added steps to set state of kioskstate var to 1 and export to currentstate.txt file

--- kiosk.sh
+++ kiosk.sh
@@ -2,6 +2,11 @@
 # this will kill all chromium browsers running
 killall chromium-browser
 sleep 10
+kioskstate=1
+export kioskstate
+echo "$kioskstate"
+echo "$kioskstate" > currentstate.txt
+echo "Set kioskstate var to $kioskstate"
 bash /home/pi/kiosk-sub.sh
 
 

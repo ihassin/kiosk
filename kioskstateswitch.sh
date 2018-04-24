01a93851e0423d8253e9aee2555f9638238bf017
shaunparris <shaunparris@gmail.com>
2018-04-23 23:35:51 -0600

    Set up original logic to execute from keypress

--- kioskstateswitch.sh
+++ kioskstateswitch.sh
@@ -1,1 +1,22 @@
-#!/usr/bin/env bash
+#! /bin/bash
+# Need this run from printscreen press
+
+kioskstate=`cat /home/pi/currentstate.txt`
+echo "Kiosk state is $kioskstate"
+if $kioskstate = 0
+then
+notify-send "Kiosk Mode Restarting"
+echo "Kiosk Mode Restarting"
+bash /home/pi/kiosk.sh
+fi
+if $kioskstate = 1
+then
+echo "Killing all bash scripts"
+kioskstate=0
+export kioskstate
+echo "Kiosk state is $kioskstate"
+echo "$kioskstate" > currentstate.txt
+echo "Set kioskstate var to $kioskstate"
+notify-send "Kiosk Mode Stopped"
+killall bash
+fi

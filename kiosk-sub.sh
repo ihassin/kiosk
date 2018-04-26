e3eb54a87dcfb93de01df656c003893880fe2673
shaunparris <shaunparris@gmail.com>
2018-04-26 12:37:43 -0600

    Trying to revert changes from yesterday, because now the main PI isn't working

--- kiosk-sub.sh
+++ kiosk-sub.sh
@@ -9,13 +9,14 @@ sleep 2
 export DISPLAY=:0.0
 
 #These settings are to prevent the "chrome crashed" messages
-sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/google-chrome/Default/Preferences
-sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/google-chrome/Default/Preferences
+#sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/google-chrome/Default/Preferences
+#sed -i 's/"exit_type": "Crashed"/"exit_type": "None"/' ~/.config/google-chrome/Default/Preferences
 
 # This next step will fire up chromium full-screen with two tabs open
 #Does this work?
+echo "Starting Chromium"
 chromium-browser --kiosk --disable-session-crashed-bubble --disable-infobars --start-maximized --noerrdialogs --enable-logging --v=1 https://trello.com/b/WJWGkyKU/buildit-us file:///home/pi/office%20presentation/assets/player/KeynoteDHTMLPlayer.html &
 #chromium-browser --kiosk --disable-session-crashed-bubble --disable-infobars --start-maximized --noerrdialogs --enable-logging --v=1 https://weather.com file:///home/pi/office%20presentation/assets/player/KeynoteDHTMLPlayer.html &
 
 sleep 5
-sudo bash /home/pi/navigation.sh
+bash /home/pi/navigation.sh
